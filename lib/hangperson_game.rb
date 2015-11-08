8014abae0f64bbe6f05998ea944032e5189b69d0
nikox94 <nikola.yuroukov@gmail.com>
2015-11-08 21:26:44 +0000

    Implemented all code for test cases

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,6 +10,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +20,43 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  
+  def guess(letter)
+    raise ArgumentError if not letter =~ /^[[:alpha:]]$/
+    letter.downcase!
+    if @word.include? letter
+      # Abort operation if letter was already guessed
+      return false if @guesses.include? letter
+      @guesses << letter
+    else
+      return false if @wrong_guesses.include? letter
+      @wrong_guesses << letter
+    end
+  end
+  
+  def word_with_guesses
+    result = ''
+    @word.each_char do |char|
+      if @guesses.include? char
+        result<<char
+      else
+        result<<'-'
+      end
+    end
+    result
+  end
+  
+  def check_win_or_lose
+    win_flag = true
+    @word.each_char do |char|
+      win_flag=false if not @guesses.include? char
+    end
+    return :win if win_flag
+    return :lose if @wrong_guesses.size>=7
+    :play
+  end
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 end
